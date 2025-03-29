94c4ff55c0f0be1b8ba5abde1a2e1cc2821cc5ba
Leonardo <leonardodebs@gmail.com>
2025-03-29 17:42:00 -0300

    mudanças

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-075686beab831bb7f"
   instance_type = "t2.micro"
   key_name = "iac-ubuntu"
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu
+                echo "<h1>Feito com Terraform</h1>" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
   tags = {
-    Name = "EC2-IAC-PrimeiraInstancia"
+    Name = "Teste AWS"
   }
 }
